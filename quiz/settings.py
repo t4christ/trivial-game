55955173049997cc0016f551af1b70d32e610575
t4christ <bakaretemitayo7@gmail.com>
2019-08-24 13:33:07 +0100

    modify settings.py file

--- quiz/settings.py
+++ quiz/settings.py
@@ -258,6 +258,7 @@ if not DEBUG:
 else:
 
     # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    PROFILE_URL='profile_photo'
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
