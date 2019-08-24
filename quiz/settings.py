fd61aecc7c85fcdf492984da9905b7642b41231d
t4christ <bakaretemitayo7@gmail.com>
2019-08-24 13:10:14 +0100

    modify settings.py file

--- quiz/settings.py
+++ quiz/settings.py
@@ -262,7 +262,7 @@ else:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
         }
@@ -270,7 +270,7 @@ else:
     STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     ]
-    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
 
 
