f4f365a1b91aa01bffbdc5d5612f7ab303713af2
t4christ <bakaretemitayo7@gmail.com>
2019-08-24 13:46:12 +0100

    modify urls.py file

--- quiz/urls.py
+++ quiz/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
