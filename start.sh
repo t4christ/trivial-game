32aff56d9d3fe82c948a5b6d3b686dba476448eb
t4christ <bakaretemitayo712@gmail.com>
2020-08-23 23:33:04 +0100

    push new script

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ echo Starting Gunicorn.
 exec gunicorn quiz.wsgi:application \
     --bind 0.0.0.0:8009 \
     --workers 1 &
+    echo Migrating Database Models
+    python manage.py migrate
     echo Starting Celery Worker &
     celery -A quiz worker -l info &
     echo Starting Celery Worker &
