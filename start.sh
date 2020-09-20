e94552422f1589779beff47c677a86966c756efd
t4christ <bakaretemitayo712@gmail.com>
2020-09-20 11:53:07 +0100

    modify views file

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 echo Starting Gunicorn.
 exec gunicorn quiz.wsgi:application \
     --bind 0.0.0.0:8009 \
-    --workers 1 &
+    --workers 3 &
     echo Migrating Database Models
     python manage.py makemigrations &
-    python manage.py migrate &
+    python manage.py migrate 
     # echo Loading Database With Questions &
     # python manage.py loaddata recharge/fixtures/recharge.json --app recharge
 
